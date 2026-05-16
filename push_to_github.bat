72a9fc9a9560c399a8759527be8dde55297ae5b9
lyy0410zh <2738288113@qq.com>
2026-05-16 09:31:16 +0800

    Delete push_to_github.bat

--- push_to_github.bat
+++
@@ -1,4 +0,0 @@
-git add .gitignore README.md train_complete.py generate_submission.py baseline_infer.py data.yaml traffic_signs_yolov8.ipynb
-git commit -m "Initial commit"
-git branch -M main
-git push -u origin main --force
